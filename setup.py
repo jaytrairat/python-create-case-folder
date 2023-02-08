ebce7fc66ed043420134d9d496eaefa4cfcf314e
jaytrairat <jaytrairat@outlook.com>
2023-02-08 11:26:50 +0700

    fix: wrong md config

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setup(
     name='create-case-folder',
-    version='0.0.6',
+    version='0.0.7',
     description='Create case folder',
     long_description=long_description,
     long_description_content_type="text/markdown",
